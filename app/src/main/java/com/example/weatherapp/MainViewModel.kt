9509431b95400599cd86186b95cd750ef6b509e9
lucrb <luc.r.brousseau@gmail.com>
2025-09-17 15:19:29 -0300

    Mainview model and weather class added.

--- app/src/main/java/com/example/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/MainViewModel.kt
@@ -2,4 +2,13 @@ package com.example.weatherapp
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    var weather : String
+
+    init {
+
+        weather = "Weather goes here."
+
+    }
+
 }
