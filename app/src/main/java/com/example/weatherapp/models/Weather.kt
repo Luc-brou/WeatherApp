015fbd8c1b3f0698d7df146aca69ec00de292ed3
lucrb <luc.r.brousseau@gmail.com>
2025-10-29 18:57:33 -0300

    Weather.kt structure set up to receive data.

--- app/src/main/java/com/example/weatherapp/models/Weather.kt
+++ app/src/main/java/com/example/weatherapp/models/Weather.kt
@@ -1,35 +1,34 @@
 package com.example.weatherapp.models
 
-import androidx.annotation.DrawableRes
 import com.google.gson.annotations.SerializedName
 
-
 data class Weather(
-//    val location: Location,
-//    val current: Current,
-//    val forecast: Forecast
+    val location: Location,
     val current: Current,
-
-    val forecast: List<Forecast>
+   val forecast: Forecast
 ) {
-
-    data class Location()
-
-    data class Current(
-        val temp_c: Double,
-        val condition: CurrentCondition,
-        val precip_mm: Double,
-        val wind_kph: Double,
-        val wind_dir: String
+    data class Location(
+        val name: String,
+        val region: String,
+        val country: String
     )
 
-    data class CurrentCondition( //couldn't have condition twice
+    data class Current(
+        @SerializedName("tempC") val currentTemp: Double,
+        val condition: CurrentCondition,
+        @SerializedName("precipMm") val current_precipitation: Double,
+        @SerializedName("windKph") val wind: Double,
+        @SerializedName("windDir") val wind_direction: String,
+        val humidity: Int
+    )
+
+    data class CurrentCondition(
         val text: String,
         val icon: String
     )
 
     data class Forecast(
-        @SerializedName("forecastday")val forecastdays: List<ForecastDay>
+        @SerializedName("forecastday") val forecastDays: List<ForecastDay>
     )
 
     data class ForecastDay(
@@ -38,42 +37,17 @@ data class Weather(
     )
 
     data class Day(
-        val maxtemp_c: Double,
-        val mintemp_c: Double,
+        val maxTempC: Double,
+        val minTempC: Double,
         val condition: Condition,
-        val totalprecip_mm: Double,
-        val daily_chance_of_rain: Int,
-        val maxwind_kph: Double,
-        val avghumidity: Int
+        val totalPrecipMm: Double,
+        val dailyChanceOfRain: Int,
+        val maxWindKph: Double,
+        val avgHumidity: Int
     )
 
     data class Condition(
         val text: String,
         val icon: String
     )
-
-//    data class Current(
-//        @DrawableRes val imageRes: Int,
-//        val condition: String,
-//        val temperature: String,
-//        val precipitationType: String,
-//        val precipitationAmount: String,
-//        val windDirection: String,
-//        val windSpeed: String
-//    )
-//
-//    data class Forecast(
-//        val date: String,
-//        @DrawableRes val imageRes: Int,
-//        val high: String,
-//        val low: String,
-//        val condition: String,
-//        val precipitationType: String,
-//        val precipitationAmount: String,
-//        val precipitationProbability: String,
-//        val windDirection: String,
-//        val windSpeed: String,
-//        val humidity: String
-//    )
-
 }
