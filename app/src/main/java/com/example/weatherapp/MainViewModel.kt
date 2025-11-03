4c125a2a89d58b967b6da8674134089c400440e1
lucrb <luc.r.brousseau@gmail.com>
2025-11-03 16:26:47 -0400

    Weather service file implemented.

--- app/src/main/java/com/example/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/MainViewModel.kt
@@ -3,17 +3,15 @@ package com.example.weatherapp
 import androidx.annotation.OptIn
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.media3.common.util.Log
 import androidx.media3.common.util.UnstableApi
 import com.example.weatherapp.models.Weather
+import com.example.weatherapp.services.WeatherApiService
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
 
 class MainViewModel : ViewModel() {
 
@@ -25,28 +23,15 @@ class MainViewModel : ViewModel() {
         .addConverterFactory(GsonConverterFactory.create())
         .build() //once appended it gets the full URL to fetch data from.
 
-    interface WeatherApiService {
-        @GET("v1/forecast.json")
-        suspend fun getForecast( //fetch data parameters
-            @Query("key") apiKey: String,
-            @Query("q") location: String,
-            @Query("days") days: Int,
-            @Query("aqi") aqi: String = "no",
-            @Query("alerts") alerts: String = "no"
-        ): Weather
-    }
-
     private val api: WeatherApiService = retrofit.create(WeatherApiService::class.java) //retrofit service creation
 
     @OptIn(UnstableApi::class)
     fun fetchWeather(apiKey: String, location: String, days: Int = 7) {
         viewModelScope.launch {
-            try {
+
                 val response = api.getForecast(apiKey, location, days)
                 _weather.value = response
-            } catch (e: Exception) { //outputs in logcat
-                Log.e("Weather", "Error fetching weather", e) //error catching if data doesnt load
-            }
+
         }
     }
 }
--- app/src/main/java/com/example/weatherapp/services/WeatherService.kt
+++ app/src/main/java/com/example/weatherapp/services/WeatherService.kt
@@ -1,4 +1,15 @@
 package com.example.weatherapp.services
+import com.example.weatherapp.models.Weather
+import retrofit2.http.GET
+import retrofit2.http.Query
 
-interface WeatherService {
+interface WeatherApiService {
+    @GET("v1/forecast.json")
+    suspend fun getForecast( //fetch data parameters
+        @Query("key") apiKey: String,
+        @Query("q") location: String,
+        @Query("days") days: Int,
+        @Query("aqi") aqi: String = "no",
+        @Query("alerts") alerts: String = "no"
+    ): Weather
 }
